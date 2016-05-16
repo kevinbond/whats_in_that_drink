3470903259c4e14f0fb7a5b7378d32e18e3b37f2
Kevin Bond <kbond@tropo.com>
2016-05-16 15:50:29 -0400

    fixed some app issues

--- whats_in_that_drink.rb
+++ whats_in_that_drink.rb
@@ -1,6 +1,47 @@
 require 'rest_client'
 require 'open-uri'
 
+def get_detail(drank)
+  text = "\u{1f379}#{drank['strDrink']}\u{1f379}:\n "
+  for i in 1..15
+    break if drank["strMeasure#{i}"] == " " || drank["strMeasure#{i}"] == "" || drank["strMeasure#{i}"] == "\n" || drank["strMeasure#{i}"] == nil
+    text = "#{text}#{drank["strMeasure#{i}"]} of #{drank["strIngredient#{i}"]}, "
+  end
+
+  text = text[0..text.length-3]
+  text = "#{text} - #{drank["strInstructions"]}"
+end
+
+def read_data(drank)
+  drank['drinks'].each do |d|
+    text = get_detail(d)
+    if text.length / 130 == 0
+      say text
+    else
+      text = text.scan(/.{1,130}/)
+      text.each do |t|
+        say t
+        wait 1500
+      end
+    end
+
+    response = ask "Would you like to see the next drink", {
+      choices: 'yes(yes, Yes, YES, y, Y, yea, yup, yeah, Yea, Yup, Yeah), no(no, No, NO, Nope, nope, nah, n, N, Nah)',
+      timeout: 3600
+    }
+    if response.value.downcase == 'no'
+      say 'Great, thank you and have a great day!'
+      hangup
+      break
+    elsif response.value.downcase == 'yes'
+      say 'Okay, grabbing the next entry'
+    else
+      say 'I did not understand that response. Goodbye'
+      hangup
+      break
+    end
+  end
+end
 
 drink = ask "", {
   choices: '[ANY]'
@@ -30,47 +71,4 @@ end
 if $currentCall.isActive
   say 'There are no more entries'
   hangup
-end
-
-
-def get_detail(drank)
-  text = "\u{1f379}#{drank['strDrink']}\u{1f379}:\n "
-  for i in 1..15
-    break if drank["strMeasure#{i}"] == " " || drank["strMeasure#{i}"] == "" || drank["strMeasure#{i}"] == "\n" || drank["strMeasure#{i}"] == nil
-    text = "#{text}#{drank["strMeasure#{i}"]} of #{drank["strIngredient#{i}"]}, "
-  end
-
-  text = text[0..text.length-3]
-  text = "#{text} - #{drank["strInstructions"]}"
-end
-
-def read_data(drank)
-  drank['drinks'].each do |d|
-    text = get_detail(d)
-    if text.length / 130 == 0
-      say text
-    else
-      text = text.scan(/.{1,130}/)
-      text.each do |t|
-        say t
-        wait 3000
-      end
-
-    end
-    response = ask "Would you like to see the next drink or are you happy with this one?", {
-      choices: 'yes(yes, Yes, YES, y, Y, yea, yup, yeah, Yea, Yup, Yeah), no(no, No, NO, Nope, nope, nah, n, N, Nah)',
-      timeout: 3600
-    }
-    if response.value.downcase == 'yes'
-      say 'Great, thank you and have a great day!'
-      hangup
-      break
-    elsif response.value.downcase == 'no'
-      say 'Okay, grabbing the next entry'
-    else
-      say 'I did not understand that response. Goodbye'
-      hangup
-      break
-    end
-  end
 end
